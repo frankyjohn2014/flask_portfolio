485127d166e783a923a8f9322f1bd9d210c4fcce
frankyjohn <frankyjohn2014@gmail.com>
2020-08-01 18:37:20 +0300

    first commit

---
+++ app/view.py
@@ -0,0 +1,9 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    name = 'Ivan'
+
+    return render_template('index.html', n=name)
