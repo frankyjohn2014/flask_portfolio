398372dd775c9ee564e816a0f8a88edede6837c6
frankyjohn <frankyjohn2014@gmail.com>
2020-08-01 22:04:56 +0300

    add static

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
 app.config.from_object(Configuration)
 
 db = SQLAlchemy(app)
--- app/view.py
+++ app/view.py
@@ -5,5 +5,4 @@ from flask import render_template
 @app.route('/', methods=['GET','POST'])
 def index():
     name = 'Ivan'
-
     return render_template('index.html', n=name)
