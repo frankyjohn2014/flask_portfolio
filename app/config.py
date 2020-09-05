939c17ebe05d9aad6dbc6442840d1501160dbd85
frankyjohn <frankyjohn2014@gmail.com>
2020-09-05 16:09:19 +0300

    add category img

--- app/config.py
+++ app/config.py
@@ -2,3 +2,5 @@ class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:3201227@localhost/sql_test'
+    STORAGE = 'static'
+    
--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from app import db
 import re
-
+import datetime
 def slugify(s):
     pattern = r'[^\w+]'
     return re.sub(pattern, '-', s)
@@ -9,6 +9,11 @@ post_category = db.Table('post_category',
                         db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
                         db.Column('category_id', db.Integer, db.ForeignKey('category.id')))
 
+import os.path as op
+
+def thumb_name(filename):
+    name, _ = op.splitext(filename)
+    return   secure_filename('%s-thumb.jpg' % name)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -28,14 +33,23 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Tag id:{}, title: {}>'.format(self.id, self.title)
-    
 
+
+# class StorageModel(db.Model):
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.Unicode(64))
+#     path = db.Column(db.Unicode(128))
+#     type = db.Column(db.Unicode(3))
+#     create_date = db.Column(db.DateTime, default=datetime.datetime.now)
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(140))
     slug =  db.Column(db.String(140), unique=True)
-
+    path = db.Column(db.Unicode(128))
+    type = db.Column(db.Unicode(3)) 
+    create_date = db.Column(db.DateTime, default=datetime.datetime.now)
 
     def __init__(self, *args, **kwargs):
         super(Category, self).__init__(*args, **kwargs)
